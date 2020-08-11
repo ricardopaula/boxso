b28371c555c2b8ac040451a3102242d60eec62ec
Ricardo Paula <ricardo.paula@redealumni.com>
2020-08-10 22:11:44 -0300

    Add nock test

--- backend/test/OrderControllerTest.js
+++ backend/test/OrderControllerTest.js
@@ -3,13 +3,15 @@ require('../src/services/GlobalBefore');
 const chai = require('chai');
 const server = require('../src/index');
 const chaiHttp = require('chai-http');
+const should = chai.should();
 const expect = chai.expect;
+const nock = require('nock')
 
 chai.use(chaiHttp);
 
 describe('Test order request', function(){
 
-const respOrder = [];
+  const respOrder = [];
 
   it('should list all orders', function(done){
     chai.request(server)
@@ -20,5 +22,47 @@ const respOrder = [];
 
         done();
       })
+  });
+});
+
+
+describe('Test order request2', function(){
+  this.beforeAll(() => {
+    const response = {
+      data: {
+        BRL_XBT: {
+          last_inexact: 1
+        }
+      }
+    }
+    nock('https://s3.amazonaws.com/response-production-walltime-info/production/')
+    .get('index/')
+    .reply(200, response);
+  })
+
+  const response2 = {
+    data: {
+      BRL_XBT: {
+        last_inexact: 1
+      }
+    }
+  }
+  nock('https://s3.amazonaws.com/data-production-walltime-info/production/dynamic')
+  .get('/walltime-info.json?now=1528962473468.679.0000000000873')
+  .reply(200, response);
+})
+
+
+  it('should list all orders', async () => {
+    resp = await chai.request(server)
+    .get('/api/orders/teste')
+    // .end(function(err, res){
+    //   res.should.have.status(200);
+    //   expect(res.body).to.deep.equal({statuss: true});
+
+    // })
+
+    resp.should.have.status(200);
+    expect(resp.body).to.deep.equal({statuss: true});
   });
 });
